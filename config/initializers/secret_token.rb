fd7f21cab82ea88d57e36cafa1cef2d10031ad69
Josue Figueroa <jfigueroagama@redesvozdatos.com>
2013-08-08 10:56:34 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kickstarter::Application.config.secret_key_base = 'fd4cdbf9be9da23df3708aeb3c2cb4c43cadde84fd2b5784a21179c333e2ea3dd97a9068ee841d32bcaba0013e07d0c7ea7d1d603dd1934b922ee1ded0e4e383'
