acda2e30a50be2d685ceea3804d8b5d36874d6e9
Josue Figueroa <jfigueroagama@redesvozdatos.com>
2013-08-11 15:18:18 -0500

    Done user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  has_secure_password
+  
   validates :name, presence: true
   validates :name, length: { maximum: 50 } 
   validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates :password, presence: true
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
